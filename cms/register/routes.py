debd4f576dc690609064c4995f0028875c27d1df
Vinícius Valle <tardelivinicius@gmail.com>
2020-02-11 14:34:26 -0300

    Added blueprint, config register.html and app.js

--- cms/register/routes.py
+++ cms/register/routes.py
@@ -1,7 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 mod = Blueprint('register', __name__, template_folder='templates')
 
 @mod.route('/')
 def index():
+    year = 2020 - 12
+    year_list = []
+    count = 0
+    for i in range(year):
+        if count == 81:
+            break
+        year_list.append(year - count)
+        count += 1
+
     return render_template('register.html')
+
+@mod.route('/save', methods=['POST'])
+def save():
+    print('email', request.form['email'])
+    print('oi, vou salvar as informações')
