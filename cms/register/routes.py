8b6dd526c364979b9ccddaa4ea781df762f0ace8
Vinícius Valle <tardelivinicius@gmail.com>
2020-02-11 17:35:32 -0300

    Created register step 1 method

--- cms/register/routes.py
+++ cms/register/routes.py
@@ -1,4 +1,6 @@
+import re
 from flask import Blueprint, render_template, request
+from flask.ext.bcrypt import Bcrypt
 
 mod = Blueprint('register', __name__, template_folder='templates')
 
@@ -15,7 +17,31 @@ def index():
 
     return render_template('register.html')
 
-@mod.route('/save', methods=['POST'])
-def save():
+@mod.route('/step1', methods=['POST'])
+def register_step1():
+
+    # E-mail Regex
+    EMAIL_REGEX = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+
+    # Validation method
+    if request.method == 'POST':
+        # E-mail Validation
+        if not EMAIL_REGEX(request.form['register-email']):
+            raise ValueError("O endereço de e-mail deve ser válido.")
+        else:
+            # CONSULTAR O E-MAIL NO BANCO PARA VER SE JÁ EXISTE, GARANTIR QUE NÃO VAI TER E-MAIL DUPLICADO
+
+        # Nickname Validation
+        if request.form['register-username'] is None
+            raise ValueError("É necessário preencher o campo usuário")
+
+        # Password Validation
+        if request.form['register-password'] is None or request.form['register-password-confirm'] is None:
+            raise ValueError("É necessário preencher os campos de senha")
+        else:
+            if request.form['register-password'] == request.form['register-password-confirm']:
+                pw_hash = bcrypt.generate_password_hash(request.form['register-password']).decode('utf-8')
+
+
     print('email', request.form['email'])
     print('oi, vou salvar as informações')
