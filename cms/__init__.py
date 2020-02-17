f60df8f0fa82217583bea2aa2a457b3432748f3b
Vinícius Valle <tardelivinicius@gmail.com>
2020-02-17 14:46:38 -0300

    New requirements.txt and __init__

--- cms/__init__.py
+++ cms/__init__.py
@@ -17,17 +17,16 @@ app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
 
 # Enviroment
-env_path = Path('.') / '.env'
 load_dotenv()
-load_dotenv(dotenv_path=env_path)
+load_dotenv(dotenv_path='config.env')
 
 # MySQL Config
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
-app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT'))
-app.config['MYSQL_UNIX_SOCKET'] = os.getenv('MYSQL_UNIX_SOCKET')
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '030595'
+app.config['MYSQL_DB'] = 'habbocms'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_UNIX_SOCKET'] = '127.0.0.1'
 app.config['MYSQL_CONNECT_TIMEOUT'] = 3000
 app.config['MYSQL_READ_DEFAULT_FILE'] = None
 app.config['MYSQL_USE_UNICODE'] = True
